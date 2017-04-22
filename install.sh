77343bed2846c7556819bec44f765320e11d2446
Adam Gilmore <adam.gilmore@outlook.com>
2017-04-22 22:53:33 +0100

    Version 1

---
+++ install.sh
@@ -0,0 +1,23 @@
+brew update
+
+brew cask install cheatsheet
+brew cask install google-chrome
+brew cask install slack
+brew cask install microsoft-office
+brew cask install skype-for-business
+
+brew cask install visual-studio-code
+brew cask install visual-studio-code-insiders
+brew install go --cross-compile-common
+brew install docker-clean
+brew install bash-completion
+
+brew update
+brew upgrade brew-cask
+brew cleanup
+
+git config --global user.name “Adam Gilmore“
+git config --global user.email adam.gilmore@outlook.com
+git config --global credential.helper osxkeychain
+git config --global core.editor "code -w"
+git config --global merge.tool code
